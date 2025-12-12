aa37d50004a0a1403fe3f5fbb58ace3abe3ce8fe
attilakronwald <attilakronwald@gmail.com>
2025-12-12 14:54:42 +0100

    ahhahaaahaaahahhaaHAHHAAHAha

--- import.py
+++ import.py
@@ -1,2 +1,74 @@
 with open('forras.txt', "r", encoding='utf-8') as fajl:
     szoveg = fajl.read()
+
+#A MEGOLDAS
+szoveg = szoveg.replace(",", "")
+szoveg = szoveg.replace(".", "")
+
+#B MEGOLDAS
+elvalasztok = [',', '.', ';', '-', '\n', '[VISSZA]', '!', ':']
+
+for jel in elvalasztok:
+    szoveg = szoveg.replace(jel, " ")
+
+#csupa kisbetu
+szoveg = szoveg.lower()
+# Szavak listaba tetele szokozok alapjan
+szavak = szoveg.split()
+
+
+
+# ell 1
+print(f"Szavak száma: {len(szavak)}")
+
+# ell2
+
+tisztitott = []
+
+for szo in szavak:
+    if szo != "":
+        tisztitott.append(szo)
+
+szavak = tisztitott
+
+#########################################
+#for x in tisztitott:
+#    print(x)
+
+
+abetu = []
+abetu.sort()
+for betu in tisztitott:
+    if "a" in betu or "A" in betu:
+        abetu.append(betu)
+
+
+print(f"Ennyi szoban szerepel az a betu: {len(abetu)}\n"
+      f"A betu szerepel a szavakban: {abetu}")
+
+with open('huzzalan.txt', 'a', encoding='utf-8')as abetus:
+    print(f"A betu van bennuk:{abetu}", file=abetus)
+
+tisztitott.count('török')
+
+torokszo = []
+bbetu = []
+cbetu = []
+dbetu = []
+
+for x in tisztitott:
+    if 'török' == x:
+        torokszo.append(x)
+    elif x[0] == "b":
+        bbetu.append(x)
+    elif x[0] == "c":
+        cbetu.append(x)
+    elif x[0] == "d":
+        dbetu.append(x)
+
+print(f"Ennyiszer szerepel a török szo a listaban:{len(torokszo)}")
+
+with open('huzzaldidi.txt', 'a', encoding='utf-8') as zalus:   
+    print(f"Ennyiszer van bcd a listaban: {len(bbetu + cbetu + dbetu)}")
+    print(f"Ennyiszer van bcd a listaban: {len(bbetu + cbetu + dbetu)}", file=zalus)
+    print(f"Ezek azok aszavak: {bbetu + cbetu + dbetu}", file=zalus)
